ae4e3400ea52eb96b07985a41bd58e46823a3890
denglitong <litong.deng@foxmail.com>
2021-08-09 20:59:11 +0800

    Chapter9 通用编程 57%
    Signed-off-by: denglitong <litong.deng@foxmail.com>

--- src/Helper.java
+++ src/Helper.java
@@ -1,13 +1,20 @@
-import java.util.EnumMap;
-import java.util.EnumSet;
+import java.time.LocalDate;
+import java.time.MonthDay;
+import java.util.Objects;
 
 /**
  * @author litong.deng@foxmail.com
  * @date 2021/7/30
  */
-public class Calculate {
+public class Helper {
 
     public static void main(String[] args) {
+        // Objects.requireNonNull(args, "args can not be null");
+
+        localDate();
+    }
+
+    private static void calculate() {
         double aScore = 7.1, bScore = 14.2, cScore = 3.55;
         int aNum = 25, bNum = 15, cNum = 10;
 
@@ -34,6 +41,33 @@ public class Calculate {
             }
         }
         System.out.println("Not Found.");
-        EnumMap.
+    }
+
+    /**
+     * https://zhuanlan.zhihu.com/p/146000139
+     */
+    private static void localDate() {
+        LocalDate today = LocalDate.now();
+        System.out.println("Today's local date: " + today);
+
+        int year = today.getYear();
+        int month = today.getMonthValue();
+        int day = today.getDayOfMonth();
+        System.out.printf("Year：%d Month：%d day: %d %n", year, month, day);
+
+        LocalDate dateOfBirth = LocalDate.of(2018, 1, 12);
+        System.out.println("The special date is: " + dateOfBirth);
+
+        System.out.println(today.equals(dateOfBirth));
+
+        MonthDay birthday = MonthDay.of(dateOfBirth.getMonthValue(), dateOfBirth.getDayOfMonth());
+        MonthDay currentMonthDay = MonthDay.from(today);
+        if (currentMonthDay.equals(birthday)) {
+            System.out.println("Many happy returns of the day !!");
+        } else {
+            System.out.println("Sorry, today is not your birthday");
+        }
+
+        // TODO more
     }
 }
